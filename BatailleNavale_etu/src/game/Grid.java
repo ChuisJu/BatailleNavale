d7d105a482ef53cb7631531813d701db9f4e3ca4
Julien Dubois <74368850+ChuisJu@users.noreply.github.com>
2021-05-06 16:04:10 +0200

    getValue reduct

--- BatailleNavale_etu/src/game/Grid.java
+++ BatailleNavale_etu/src/game/Grid.java
@@ -25,22 +25,7 @@ public class Grid {
 	}
 	
     public int getValue(int column, int line) {
-		int j = 0;
-		int i = 0;
-		boolean boucle = true;
-		for (i=0; boucle && i<10; i++) {
-			for (j=0; j<10; j++) {
-				if(i == column && j == line) {
-					if(mat[i][j]==1) {
-						boucle = false;
-					}
-				}
-			}
-		}
-		if (boucle == false) {
-			return 1;
-		}
-		return 0;
+		return mat[line][column];
 	}
 	
 	public void addShot(int column, int line, boolean success) {
