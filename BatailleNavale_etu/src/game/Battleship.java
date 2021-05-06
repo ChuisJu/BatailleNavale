560526653042e3f1eeec93e6b708d9f2a679154c
guillaume77120 <75263333+guillaume77120@users.noreply.github.com>
2021-05-06 17:32:13 +0200

    Update Battleship.java

--- BatailleNavale_etu/src/game/Battleship.java
+++ BatailleNavale_etu/src/game/Battleship.java
@@ -1,17 +1,21 @@
 package game;
 
 public class Battleship {
-	public static void main(String[] args) {
-		boolean replay;
-		Game.launch2players();
-		do {
-			replay = Game.shot();
-			if(!replay) {
-				System.out.println("Raté !");
-				Game.changeCurrentPlayer();
-			}else {
-				System.out.println("Touché ! Vous pouvez rejouer.");
-			}
-		}while(!Game.isOver());
-	}
+    public static void main(String[] args) {
+        boolean replay;
+        Game.launch2players();
+        do {
+            replay = Game.shot();
+            if(!replay) {
+                System.out.println("Raté !");
+                Game.changeCurrentPlayer();
+            }else if (!Game.isOver()){
+                System.out.println("Touché ! Vous pouvez rejouer.");
+            }
+            else {
+                System.out.println("Touché !");
+            }
+        }while(!Game.isOver());
+        System.out.println("Partie terminée ! Merci d'avoir joué !");
+    }
 }
